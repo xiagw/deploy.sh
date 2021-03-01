58b6430fc8ee7bcb29530499f1d667c5aec05a2c
xiagw <fxiaxiaoyu@gmail.com>
2021-03-01 21:46:10 +0800

    install helm and flyway

--- flyway/install-flyway.sh
+++ flyway/install-flyway.sh
@@ -2,4 +2,7 @@
 
 # Command-line - Command-line tool - Flyway by Redgate • Database Migrations Made Easy.
 # https://flywaydb.org/documentation/usage/commandline/#download-and-installation
-wget -qO- https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/7.0.3/flyway-commandline-7.0.3-linux-x64.tar.gz | tar xvz && sudo ln -s "$(pwd)"/flyway-7.0.3/flyway /usr/local/bin
+
+# flyway_ver=7.5.4
+flyway_ver="$(curl -sSL 'https://flywaydb.org/documentation/usage/commandline/#download-and-installation' | grep -oP -m1 '(?<=flyway-commandline-)\d+\.\d+\.\d+' | head -n 1)"
+wget -qO- https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/"$flyway_ver"/flyway-commandline-"$flyway_ver"-linux-x64.tar.gz | tar xvz && sudo ln -s "$(pwd)"/flyway-"$flyway_ver"/flyway /usr/local/bin
--- helm/install-helm.sh
+++ helm/install-helm.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-curl
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
