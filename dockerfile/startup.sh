02e4545ed8629dc7213e20404d56d1b9182553ad
xiagw <fxiaxiaoyu@gmail.com>
2021-03-18 21:08:32 +0800

    add startup.sh and autostart.env

--- dockerfile/startup.sh
+++ dockerfile/startup.sh
@@ -1,32 +1,31 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC1090,SC1091
 
-swoole_task() {
-    echo "[ThinkPHP] Starting swoole..."
-    while IFS= read -r line; do
-        php think "$line" start
-    done < <(cat autostart.swoole.task)
-
-}
 cron_task() {
     echo "[ThinkPHP] Starting cron..."
     while true; do
-        while IFS= read -r line; do
-            php think "$line" &
-        done < <(cat autostart.cron.task)
+        for j in ${task_cron}; do
+            php think "$j" &
+        done
         sleep 60
     done
 }
 
 main() {
-    ## if used ThinkPHP
-    ## if you want run crontab or swoole task
+    ## if you want run crontab or swoole task in ThinkPHP
     for f in /var/www/*/think; do
         path_app=${f%/*}
         cd "${path_app}" || exit 1
-        if [[ -f "${path_app}/autostart.swoole" && -f "${path_app}/autostart.swoole.task" ]]; then
-            swoole_task
-        elif [[ -f "${path_app}/autostart.cron" && -f "${path_app}/autostart.cron.task" ]]; then
-            cron_task &
+
+        if [[ -f 'autostart.env' ]]; then
+            source 'autostart.env'
+            if [[ ${autostart_swoole:-0} == 'true' ]]; then
+                echo "[ThinkPHP] Starting swoole..."
+                php think "${task_swoole:-SwooleTask}" start
+            fi
+            if [[ ${autostart_cron:-0} == 'true' ]]; then
+                cron_task &
+            fi
         else
             echo '[ThinkPHP] Nothing to do.'
         fi
