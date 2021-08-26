481b57813664b3e36a493b3ac68d8df8ee02abb8
xiagw <fxiaxiaoyu@gmail.com>
2021-08-26 17:54:45 +0800

    install eksctl

--- deploy.sh
+++ deploy.sh
@@ -550,6 +550,9 @@ install_aws() {
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     unzip -qq awscliv2.zip
     sudo ./aws/install
+    ## install eksctl
+    curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+    sudo mv /tmp/eksctl /usr/local/bin
 }
 
 install_kubectl() {
