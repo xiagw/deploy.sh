8cfec54ad62335665fc0f1f3f633cf976f1b8fce
xiagw <fxiaxiaoyu@gmail.com>
2021-10-28 14:15:45 +0800

    🐞 fix(deploy.sh): helm envNamespace

--- deploy.sh
+++ deploy.sh
@@ -403,6 +403,7 @@ deploy_k8s_generic() {
             --set image.repository="${ENV_DOCKER_REGISTRY}/${ENV_DOCKER_REPO}" \
             --set image.tag="${image_tag}" \
             --set nfsServer="${ENV_NFS_SERVER:?undefine}" \
+            --set envNamespace="${k8s_namespace:-$CI_COMMIT_REF_NAME}" \
             --set image.pullPolicy='Always' >/dev/null
     fi
     kubectl -n "${k8s_namespace:-$CI_COMMIT_REF_NAME}" get replicasets.apps | awk '/replicasets.apps.*0\s+0\s+0/{print $1}' | xargs kubectl -n "${k8s_namespace:-$CI_COMMIT_REF_NAME}" delete replicasets.apps >/dev/null 2>&1 || true
