d059c8f55359474494da410cb69dd3bd3ee09624
xiagw <fxiaxiaoyu@gmail.com>
2021-12-19 23:54:46 +0800

    🐞 fix\( M langs/build.docker.sh  M langs/build.java.sh\): build java

--- langs/build.docker.sh
+++ langs/build.docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-DOCKER_BUILDKIT=1 docker build ${quiet_flag} --tag "${ENV_DOCKER_REGISTRY}/${ENV_DOCKER_REPO}:${gitlab_project_name}-${gitlab_commit_short_sha}" \
+DOCKER_BUILDKIT=1 docker build ${quiet_flag} --tag "${ENV_DOCKER_REGISTRY}/${ENV_DOCKER_REPO}:${image_tag}" \
     --build-arg CHANGE_SOURCE="${ENV_CHANGE_SOURCE:-false}" "${gitlab_project_dir}"
 echo_time "end docker build."
--- langs/build.java.sh
+++ langs/build.java.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-echo_time_step "java maven build..."
-mvn clean install
+if [[ -f $gitlab_project_dir/build.gradle ]]; then
+    echo_time_step "java build [gradle]..."
+    gradle -q
+else
+    echo_time_step "java build [maven]..."
+    mvn clean install
+fi
