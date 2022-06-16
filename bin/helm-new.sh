ddfa11433fde9282520890dca4b30e645a9df0a1
xiagw <fxiaxiaoyu@gmail.com>
2022-06-16 21:01:56 +0800

    🐞fix(M  bin/helm-new.sh M  bin/ssh-port-forward.sh)

--- bin/helm-new.sh
+++ bin/helm-new.sh
@@ -2,27 +2,38 @@
 
 set -xe
 
-script_path="$(cd "$(dirname "$0")" && pwd)"
-# script_path="$(dirname "$(readlink -f "$0")")"
-[ -d "$script_path/../data/helm" ] || mkdir -p "$script_path/../data/helm"
-cd "$script_path/../data/helm" || exit 1
+script_path="$(dirname "$(readlink -f "$0")")"
+cd "$script_path" || exit 1
 
-read -rp "release name? " -e -i 'backend-' release_name
-# read -rp "prefix name? " -e prefix_name
-read -rp "port number? " -e -i '8080' port_number
-read -rp "http or tcp? " -e -i 'http' protocol
+if [[ -z "$1" ]]; then
+    ## 获取 release名称 端口 协议信息
+    read -rp "release name? " -e -i 'nginx' release_name
+    read -rp "port number? " -e -i '80' port_number
+    read -rp "http or tcp? " -e -i 'tcp' protocol
+else
+    release_name="${1:-nginx}"
+    port_number="${2:-80}"
+    protocol="${3:-http}"
+fi
 
-## create helm chart
-helm create "$release_name"
-## change helm chart values
+## 创建 helm chart
+helm create "${release_name:?empty var}"
+
+## change values.yaml
 sed -i \
-    -e "s@port: 80@port: ${port_number}@" \
+    -e "s@port: 80@port: ${port_number:-80}@" \
     "$release_name/values.yaml"
+## change service.yaml
 sed -i \
     -e "s@targetPort: http@targetPort: {{ .Values.service.port }}@" \
     "$release_name/templates/service.yaml"
+## change serviceaccount.yaml
+sed -i \
+    -e "/serviceAccountName/s/^/#/" "$release_name/templates/deployment.yaml"
+## remove serviceaccount.yaml
+rm -f "$release_name/templates/serviceaccount.yaml"
 
-if [[ $protocol == 'http' ]]; then
+if [[ "${protocol:-http}" == 'http' ]]; then
     sed -i \
         -e "s@containerPort: 80@containerPort: {{ .Values.service.port }}@" \
         -e "s@port: http@port: {{ .Values.service.port }}@g" \
--- bin/ssh-port-forward.sh
+++ bin/ssh-port-forward.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
 
 # set -x
-
-path0_script="$(cd "$(dirname "$0")" && pwd)"
-echo "$path0_script" >/dev/null
-
 command -v nc >/dev/null 2>&1 || {
     echo >&2 "I require nc but it's not installed.  Aborting."
     return 1
 }
-
 ssh_opt='ssh -qfg -o ExitOnForwardFailure=yes'
 ssh_host='ip.host.mysql.server'
 ssh_forward_port='3306'
+## create ssh tunnel
 $ssh_opt -L $ssh_forward_port:localhost:3306 $ssh_host "sleep 120" || true
-
+## kill ssh tunnel timeout
 while nc -vz localhost $ssh_forward_port; do
     # echo "ssh forward port $ssh_forward_port exist."
     count=$((count + 1))
