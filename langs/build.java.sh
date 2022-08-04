4476459f294df8a2afe8a25641d735b1c1ed86cb
xiagw <fxiaxiaoyu@gmail.com>
2022-08-04 09:26:44 +0800

    build java mvn --quiet

--- langs/build.java.sh
+++ langs/build.java.sh
@@ -9,12 +9,14 @@ if [[ -f "$gitlab_project_dir/build.gradle" ]]; then
 else
     echo_msg step "java build [maven]..."
     # docker run -i --rm -v "$gitlab_project_dir":/usr/src/mymaven -w /usr/src/mymaven maven:3.6-jdk-8 mvn clean -U package -DskipTests
-    docker run -i --rm -v "$gitlab_project_dir":/usr/src/mymaven \
-        -w /usr/src/mymaven maven:3.6-jdk-8 mvn clean -U package -DskipTests --activate-profiles "$MVN_PROFILE"
+    docker run -i --rm \
+        -v "$gitlab_project_dir":/usr/src/mymaven \
+        -v "$gitlab_project_dir"/docs/settings.xml:/root/.m2/settings.xml \
+        -w /usr/src/mymaven maven:3.6-jdk-8 \
+        mvn clean -U package -DskipTests --quiet --activate-profiles "$MVN_PROFILE"
 fi
 
 [ -d $path_for_rsync ] || mkdir "$gitlab_project_dir/${path_for_rsync%/}"
 
-find "${gitlab_project_dir}" -path "./${path_for_rsync%/}" -prune -o -type f \
+find "${gitlab_project_dir}" -path "${gitlab_project_dir}/${path_for_rsync%/}" -prune -o -type f \
     -regextype egrep -iregex '.*SNAPSHOT.*\.jar' -exec cp {} "$gitlab_project_dir/$path_for_rsync" \;
-
