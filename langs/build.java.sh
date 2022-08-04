9592dea34cec0f801563ef503a74a7c2420fc074
xiagw <fxiaxiaoyu@gmail.com>
2022-08-04 18:07:11 +0800

    have dockerfile then disable mvn build

--- langs/build.java.sh
+++ langs/build.java.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [[ "${project_docker}" -eq 1 ]]; then
+    return 0
+fi
+
 path_for_rsync='jar_file/'
 MVN_PROFILE="${gitlab_project_branch}"
 
@@ -13,7 +17,7 @@ else
         -v "$gitlab_project_dir":/app \
         -w /app \
         maven:3.6-jdk-8 \
-        mvn -T 1C clean --quiet  \
+        mvn -T 1C clean --quiet \
         --update-snapshots package \
         --define skipTests \
         --define maven.compile.fork=true \
