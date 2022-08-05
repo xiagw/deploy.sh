14b0e7e45490669eb1d440264e6042e769c1b601
xiagw <fxiaxiaoyu@gmail.com>
2022-08-05 10:19:47 +0800

    fix java build

--- langs/build.java.sh
+++ langs/build.java.sh
@@ -13,15 +13,18 @@ if [[ -f "$gitlab_project_dir/build.gradle" ]]; then
 else
     echo_msg step "java build [maven]..."
     # docker run -i --rm -v "$gitlab_project_dir":/usr/src/mymaven -w /usr/src/mymaven maven:3.6-jdk-8 mvn clean -U package -DskipTests
-    [ -d "$gitlab_project_dir"/maven_repository ] || mkdir "$gitlab_project_dir"/maven_repository
-    docker run -i --rm -u 1000:1000 \
-        -v "$gitlab_project_dir"/maven_repository:/maven_repository \
-        -v "$gitlab_project_dir":/app \
+    [ -d "${script_path_data}"/maven/.m2 ] || mkdir -p "${script_path_data}"/maven/.m2
+
+    docker run -i --rm --user "$(id -u):$(id -g)" \
+        -e MAVEN_CONFIG=/var/maven/.m2 \
+        -v "$script_path_data"/maven:/var/maven/.m2:rw \
+        -v "$gitlab_project_dir":/app:rw \
         -w /app \
         maven:3.6-jdk-8 \
         mvn -T 1C clean --quiet \
         --update-snapshots package \
         --define skipTests \
+        --define user.home=/var/maven \
         --define maven.compile.fork=true \
         --activate-profiles "$MVN_PROFILE" \
         --settings settings.xml
@@ -30,4 +33,4 @@ fi
 [ -d $path_for_rsync ] || mkdir "$gitlab_project_dir/${path_for_rsync%/}"
 
 find "${gitlab_project_dir}" -path "${gitlab_project_dir}/${path_for_rsync%/}" -prune -o -type f \
-    -regextype egrep -iregex '.*SNAPSHOT.*\.jar' -exec cp {} "$gitlab_project_dir/$path_for_rsync" \;
+    -regextype egrep -iregex '.*SNAPSHOT.*\.(jar|war)' -exec cp {} "$gitlab_project_dir/$path_for_rsync" \;
