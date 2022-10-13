05f4d9ed34da5f45629e64c3ee1a3f061714f179
xiagw <fxiaxiaoyu@gmail.com>
2022-10-13 14:11:41 +0800

    fix send weixin sh

--- bin/send.weixin.sh
+++ bin/send.weixin.sh
@@ -1,51 +1,40 @@
 #!/usr/bin/env bash
 
-# 微信消息发送脚本 zhutw
-#全局配置--
-#微信公众号appID
-appID='wx*******0ebde756'
-#微信公众号appsecret
-appsecret='138********0446e9ae04f2'
-#微信公众号发送消息模板
-tpl_id='OA0PX8pqc2X7t_-y05y5GxZ8LutBpu341FIYSeQOkno'
-#消息模板：
-#   {{first.DATA}}
-#   项目名称：{{name.DATA}}
-#   报警时间：{{date.DATA}}
-#
-#   {{remark.DATA}}
-
 #获取微信公众号AccessToken，并缓存到本地 函数
+get_token_cache() {
+    echo "get from local cache $file_local_cache"
+    access_token=$(awk -F":" '{print $1}' "$file_local_cache")
+    expires_in=$(awk -F":" '{print $2}' "$file_local_cache")
+    token_time=$(awk -F":" '{print $3}' "$file_local_cache")
+}
+get_token_online() {
+    echo "get from online https://api.weixin.qq.com"
+    content=$(curl "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$appID&secret=$appsecret")
+    access_token=$(echo "$content" | awk -F "\"" '{print $4}')
+    expires_in=$(echo "$content" | awk -F "\"" '{print $7}' | cut -d"}" -f1 | cut -c2-)
+    token_time=$(date +%s)
+    echo "get content: $content"
+    echo "access_token = $access_token"
+    echo "expires_in = $expires_in"
+    echo "$access_token:$expires_in:$token_time" >"$file_local_cache"
+}
 getAccessToken() {
-    if [ -f "$HOME/.wechat_accesstoken" ]; then
-        access_token=$(awk -F":" '{print $1}' $HOME/.wechat_accesstoken)
-        expires_in=$(awk -F":" '{print $2}' $HOME/.wechat_accesstoken)
-        time=$(awk -F":" '{print $3}' $HOME/.wechat_accesstoken)
-        if [ -z $access_token ] || [ -z $expires_in ] || [ -z $time ]; then
-            rm -f $HOME/.wechat_accesstoken
-            getAccessToken
-        fi
-    else
-        content=$(curl "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$appID&secret=$appsecret")
-        access_token=$(echo $content | awk -F "\"" '{print $4}')
-        expires_in=$(echo $content | awk -F "\"" '{print $7}' | cut -d"}" -f1 | cut -c2-)
-        time=$(date +%s)
-        echo "get content: $content"
-        echo "access_token = $access_token"
-        echo "expires_in = $expires_in"
-        echo "$access_token:$expires_in:$time" >$HOME/.wechat_accesstoken
-        if [ -z $access_token ] || [ -z $expires_in ] || [ -z $time ]; then
-            echo "not get access_token"
-            exit 0
-        fi
+    file_local_cache="$HOME/.wechat_accesstoken"
+    if [ -f "$file_local_cache" ]; then
+        get_token_cache
     fi
-    remain=$(($(date +%s) - $time))
+    remain=$(($(date +%s) - $token_time))
     limit=$(($expires_in - 60))
-    if [ $remain -gt $limit ]; then
-        rm -f $HOME/.wechat_accesstoken
-        getAccessToken
+    if [ -z "$access_token" ] || [ -z "$expires_in" ] || [ -z "$token_time" ] || [ $remain -gt $limit ]; then
+        rm -f "$file_local_cache"
+        get_token_online
+    fi
+    if [ -z "$access_token" ] || [ -z "$expires_in" ] || [ -z "$token_time" ]; then
+        echo "fail to get access_token"
+        exit 0
     fi
 }
+
 #发送消息函数
 sendMessage() {
     #消息json体
@@ -77,7 +66,9 @@ sendMessage() {
 EOF
     )
     echo "send message : $message"
-    curl -X POST -H "Content-Type: application/json" https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=$access_token -d "$message"
+    curl -X POST -H "Content-Type: application/json" \
+        https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=$access_token \
+        -d "$message"
 }
 
 #帮助信息函数
@@ -94,47 +85,65 @@ usage: $0 [-u openids -s summary -n name -t time -d detail -l link] [-h]
 EOF
 }
 
-#获取脚本执行参数
-while getopts ":u:s:n:t:d:h:l:" op; do
-    case $op in
-    u)
-        openids="$OPTARG"
-        ;;
-    s)
-        first="$OPTARG"
-        ;;
-    n)
-        name="$OPTARG"
-        ;;
-    t)
-        date="$OPTARG"
-        ;;
-    d)
-        remark="$OPTARG"
-        ;;
-    l)
-        url="$OPTARG"
-        ;;
-    *)
-        usage
-        exit 0
-        ;;
-    esac
-done
+main() {
+    # 微信消息发送脚本
+    #全局配置--
+    #微信公众号appID
+    appID='wx*******0ebde756'
+    #微信公众号appsecret
+    appsecret='138********0446e9ae04f2'
+    #微信公众号发送消息模板
+    tpl_id='OA0PX8pqc2X7t_-y05y5GxZ8LutBpu341FIYSeQOkno'
+    #消息模板：
+    #   {{first.DATA}}
+    #   项目名称：{{name.DATA}}
+    #   报警时间：{{date.DATA}}
+    #
+    #   {{remark.DATA}}
 
-#判断条件满足发送消息
-if [[ -n $openids && -n $first && -n $name && -n $date ]]; then
-    getAccessToken
-    OLD_IFS="$IFS"
-    IFS=","
-    arr=($openids)
-    IFS="$OLD_IFS"
-    for openid in ${arr[@]}; do
-        sendMessage
+    #获取脚本执行参数
+    while getopts ":u:s:n:t:d:h:l:" op; do
+        case $op in
+        u)
+            openids="$OPTARG"
+            ;;
+        s)
+            first="$OPTARG"
+            ;;
+        n)
+            name="$OPTARG"
+            ;;
+        t)
+            date="$OPTARG"
+            ;;
+        d)
+            remark="$OPTARG"
+            ;;
+        l)
+            url="$OPTARG"
+            ;;
+        *)
+            usage
+            return 0
+            ;;
+        esac
     done
-    exit $?
-else
-    echo "params error."
-    usage
-    exit 1
-fi
+
+    #判断条件满足发送消息
+    if [[ -n $openids && -n $first && -n $name && -n $date ]]; then
+        getAccessToken
+        OLD_IFS="$IFS"
+        IFS=","
+        IFS="$OLD_IFS"
+        for openid in $openids; do
+            sendMessage
+        done
+        return $?
+    else
+        echo "params error."
+        usage
+        return 1
+    fi
+}
+
+main "$@"
