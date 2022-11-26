c2bab22cabe2c30a5a43aaa0a395dc082979553e
xiagw <fxiaxiaoyu@gmail.com>
2022-11-26 23:32:32 +0800

    fix dockerfile/run.sh

--- conf/dockerfile/run.sh
+++ conf/dockerfile/run.sh
@@ -5,8 +5,9 @@ _start_jar() {
     if [ -z "$JAVA_OPTS" ]; then
         JAVA_OPTS='java -Xms256m -Xmx384m'
     fi
-    ## 自动探测环境变量，默认值 profile.test，(Dockerfile ARG MVN_PROFILE=test)
-    ## Dockerfile 内生成文件 profile.test, profile.main 等分支名结尾
+    ## 启动方式一， jar 内置配置文件 yml，
+    ## Dockerfile ARG MVN_PROFILE=test （此处对应 git 分支名）
+    ## Dockerfile 镜像内生成文件 profile.分支名
     for f in "$me_path"/profile.*; do
         [[ -f "$f" ]] || continue
         echo "found $f"
@@ -18,6 +19,7 @@ _start_jar() {
         [[ -f "$jar" ]] || continue
         cj=$((cj + 1))
         cy=0
+        ## 启动方式二，配置文件 yml 在 jar 包外，非内置
         ## !!!! 注意 !!!!,
         ## 自动探测 yml 配置文件, 按文件名自动排序对应 a.jar--a.yml, b.jar--b.yml
         for y in "$me_path"/*.yml; do
@@ -25,26 +27,27 @@ _start_jar() {
             cy=$((cy + 1))
             [[ "$cj" -eq "$cy" ]] && config_yml="-Dspring.config.location=${y}"
         done
-        if [ -z "$profile_name" ]; then
+        if [ -n "$profile_name" ]; then
             echo "${cj}. start $jar with $profile_name ..."
             $JAVA_OPTS -jar "$jar" $profile_name &
         else
             echo "${cj}. start $jar with $config_yml ..."
             $JAVA_OPTS $config_yml -jar "$jar" &
         fi
-        java_pids="${java_pids} $!"
+        pids="${pids} $!"
     done
-    ## allow debug / 方便开发者调试，可以直接 kill java, 不会停止容器
-    tail -f "$me_log" "$me_path"/log/*.log &
 }
 
 _start_php() {
-    ## 容器内代替 cronta
+    ## 容器内代替 crontab
     [ -f /var/www/schedule.sh ] && bash /var/www/schedule.sh &
     [ -f "$me_path"/schedule.sh ] && bash "$me_path"/schedule.sh &
     if [ -f easyswoole ]; then
+        ## 前台启动需配置到 config 文件内
         exec php easyswoole server start -mode=config
+        pids="${pids} $!"
     elif command -v php-fpm >/dev/null 2>&1; then
+        ## 前台启动
         exec php-fpm -F
     else
         echo "Give up php."
@@ -53,7 +56,7 @@ _start_php() {
 
 _kill() {
     echo "[INFO] Receive SIGTERM"
-    for pid in $java_pids; do
+    for pid in $pids; do
         kill "$pid"
         wait "$pid"
     done
@@ -63,14 +66,16 @@ main() {
     me_path="$(dirname "$(readlink -f "$0")")"
     me_name="$(basename "$0")"
     me_log="${me_path}/${me_name}.log"
-    date >>"$me_log"
+    echo "$(date), start..." | tee -a "$me_log"
     [ -d "$me_path"/log ] || mkdir "$me_path"/log
     ## 识别中断信号，停止 java 进程
     trap _kill HUP INT QUIT TERM
-    ## start php
+    ## 统一兼容启动 start php
     _start_php
-    ## 启动 java
+    ## 统一兼容启动 start java
     _start_jar
+    ## allow debug / 方便开发者调试，可以直接 kill java, 不会停止容器
+    tail -f "$me_log" "$me_path"/log/*.log &
     ## 适用于 docker 中启动
     wait
 }
