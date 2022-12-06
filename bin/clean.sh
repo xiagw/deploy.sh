92aeba478f33c0ae2e9eab640f94b540ab17c916
xiagw <fxiaxiaoyu@gmail.com>
2022-12-06 11:00:39 +0800

    fix bin/sh

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-#Removes old revisions of snaps
-#CLOSE ALL SNAPS BEFORE RUNNING THIS
-set -eu
+# set -eu
+
+## Removes old revisions of snaps
+## CLOSE ALL SNAPS BEFORE RUNNING THIS
 LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
     while read -r snapname revision; do
         sudo snap remove "$snapname" --revision="$revision"
     done
+
+## clean thinkphp runtime/log
+
+## clean mysql binary logs
+
+## clean
