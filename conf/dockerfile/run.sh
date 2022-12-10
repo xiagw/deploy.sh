3ef8b8f841361088c34179bf8f84aa689c6d18ec
xiagw <fxiaxiaoyu@gmail.com>
2022-12-10 21:22:07 +0800

    run.sh /app

--- conf/dockerfile/run.sh
+++ conf/dockerfile/run.sh
@@ -3,20 +3,20 @@
 _start() {
     ## 修改内存占用值，
     [ -z "$JAVA_OPTS" ] && JAVA_OPTS='java -Xms256m -Xmx384m'
-    [[ "$1" == nohup || -f "$me_path"/.run.nohup ]] && JAVA_OPTS="nohup $JAVA_OPTS"
+    [[ "$1" == nohup || -f "$app_path"/.run.nohup ]] && JAVA_OPTS="nohup $JAVA_OPTS"
     ## 启动方式一， jar 内置配置文件 yml，
     ## Dockerfile ARG MVN_PROFILE=test （此处对应 git 分支名） 镜像内生成文件 profile.<分支名>
-    for f in "$me_path"/profile.*; do
+    for f in "$app_path"/profile.*; do
         [[ -f "$f" ]] || continue
         profile_name="--spring.profiles.active=${f##*.}"
         break
     done
-    for jar in "$me_path"/*.jar; do
+    for jar in "$app_path"/*.jar; do
         [[ -f "$jar" ]] || continue
         cj=$((${cj:-0} + 1))
         ## 启动方式二，配置文件 yml 在 jar 包外，非内置
         ## !!!! 注意 !!!!, 自动探测 yml 配置文件, 按文件名自动排序对应 a.jar--a.yml, b.jar--b.yml
-        for y in "$me_path"/*.yml; do
+        for y in "$app_path"/*.yml; do
             [[ -f "$y" ]] || continue
             cy=$((${cy:-0} + 1))
             [[ "$cj" -eq "$cy" ]] && config_yml="-Dspring.config.location=${y}"
@@ -36,7 +36,7 @@ _start() {
 _start_php() {
     ## 容器内代替 crontab
     [ -f /var/www/schedule.sh ] && bash /var/www/schedule.sh &
-    [ -f "$me_path"/schedule.sh ] && bash "$me_path"/schedule.sh &
+    [ -f "$app_path"/schedule.sh ] && bash "$app_path"/schedule.sh &
     if [ -f easyswoole ]; then
         ## 前台启动需配置到 config 文件内
         exec php easyswoole server start -mode=config
@@ -64,17 +64,19 @@ main() {
     me_name="$(basename "$0")"
     me_path="$(dirname "$(readlink -f "$0")")"
     me_log="${me_path}/${me_name}.log"
-    echo "$(date), start..." | tee -a "$me_log"
-    [ -d "$me_path"/log ] || mkdir "$me_path"/log
+    app_path=/app
+    app_log=$app_path/${me_name}.log
+    echo "$(date), start..." | tee -a "$app_log"
+    [ -d "$app_path"/log ] || mkdir "$app_path"/log
     ## 识别中断信号，停止 java 进程
     trap _kill HUP INT QUIT TERM
     _start_php
     ## 启动 java
     _start "$@"
     ## allow debug / 方便开发者调试，可以直接 kill java, 不会停止容器
-    tail -f "$me_log" "$me_path"/log/*.log &
+    tail -f "$me_log" "$app_path"/log/*.log &
     ## 适用于 docker 中启动
-    if [[ -z "$1" || ! -f "$me_path"/.run.nohup ]]; then
+    if [[ -z "$1" || ! -f "$app_path"/.run.nohup ]]; then
         wait
     fi
 }
