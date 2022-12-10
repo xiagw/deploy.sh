03d2c8a40f54f299e140a633ac63e7cd8e451dcf
xiagw <fxiaxiaoyu@gmail.com>
2022-12-10 21:08:20 +0800

    fix run.sh

--- conf/dockerfile/run.sh
+++ conf/dockerfile/run.sh
@@ -1,44 +1,33 @@
 #!/usr/bin/env bash
 
-_start_java() {
+_start() {
     ## 修改内存占用值，
-    if [ -z "$JAVA_OPTS" ]; then
-        JAVA_OPTS='java -Xms256m -Xmx384m'
-    fi
-    if [[ "$1" == nohup ]]; then
-        JAVA_OPTS="nohup $JAVA_OPTS"
-    fi
+    [ -z "$JAVA_OPTS" ] && JAVA_OPTS='java -Xms256m -Xmx384m'
+    [[ "$1" == nohup || -f "$me_path"/.run.nohup ]] && JAVA_OPTS="nohup $JAVA_OPTS"
     ## 启动方式一， jar 内置配置文件 yml，
-    ## Dockerfile ARG MVN_PROFILE=test （此处对应 git 分支名）
-    ## Dockerfile 镜像内生成文件 profile.<分支名>
+    ## Dockerfile ARG MVN_PROFILE=test （此处对应 git 分支名） 镜像内生成文件 profile.<分支名>
     for f in "$me_path"/profile.*; do
         [[ -f "$f" ]] || continue
-        echo "found $f"
         profile_name="--spring.profiles.active=${f##*.}"
         break
     done
-    cj=0
     for jar in "$me_path"/*.jar; do
         [[ -f "$jar" ]] || continue
-        cj=$((cj + 1))
-        cy=0
+        cj=$((${cj:-0} + 1))
         ## 启动方式二，配置文件 yml 在 jar 包外，非内置
-        ## !!!! 注意 !!!!,
-        ## 自动探测 yml 配置文件, 按文件名自动排序对应 a.jar--a.yml, b.jar--b.yml
+        ## !!!! 注意 !!!!, 自动探测 yml 配置文件, 按文件名自动排序对应 a.jar--a.yml, b.jar--b.yml
         for y in "$me_path"/*.yml; do
             [[ -f "$y" ]] || continue
-            cy=$((cy + 1))
+            cy=$((${cy:-0} + 1))
             [[ "$cj" -eq "$cy" ]] && config_yml="-Dspring.config.location=${y}"
         done
+        echo "${cj}. start $jar ..."
         if [ -n "$profile_name" ]; then
-            echo "${cj}. start $jar with $profile_name ..."
-            $JAVA_OPTS -jar "$jar" $profile_name &>>$me_log &
+            $JAVA_OPTS -jar "$jar" "$profile_name" &>>"$me_log" &
         elif [ -n "$config_yml" ]; then
-            echo "${cj}. start $jar with $config_yml ..."
-            $JAVA_OPTS $config_yml -jar "$jar" &>>$me_log &
+            $JAVA_OPTS "$config_yml" -jar "$jar" &>>"$me_log" &
         else
-            echo "${cj}. start $jar ..."
-            $JAVA_OPTS -jar "$jar" &>>$me_log &
+            $JAVA_OPTS -jar "$jar" &>>"$me_log" &
         fi
         pids="$pids $!"
     done
@@ -59,7 +48,7 @@ _start_php() {
         exec php-fpm
         pids="${pids} $!"
     else
-        echo "Give up php."
+        echo "Not found php."
     fi
 }
 
@@ -72,21 +61,20 @@ _kill() {
 }
 
 main() {
-    me_path="$(dirname "$(readlink -f "$0")")"
     me_name="$(basename "$0")"
+    me_path="$(dirname "$(readlink -f "$0")")"
     me_log="${me_path}/${me_name}.log"
     echo "$(date), start..." | tee -a "$me_log"
     [ -d "$me_path"/log ] || mkdir "$me_path"/log
     ## 识别中断信号，停止 java 进程
     trap _kill HUP INT QUIT TERM
-    ## 统一兼容启动 start php
     _start_php
-    ## 统一兼容启动 start java
-    _start_java $1
+    ## 启动 java
+    _start "$@"
     ## allow debug / 方便开发者调试，可以直接 kill java, 不会停止容器
     tail -f "$me_log" "$me_path"/log/*.log &
     ## 适用于 docker 中启动
-    if [[ -z "$1" ]]; then
+    if [[ -z "$1" || ! -f "$me_path"/.run.nohup ]]; then
         wait
     fi
 }
