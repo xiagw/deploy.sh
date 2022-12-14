2fa37ad6fb10747cbe5291f788da8f62901a1d1d
xiagw <fxiaxiaoyu@gmail.com>
2022-12-14 22:50:47 +0800

    fix clean.sh

--- bin/clean.sh
+++ bin/clean.sh
@@ -10,6 +10,12 @@ LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
     done
 
 ## clean thinkphp runtime/log
+find . -type d -name runtime |
+    while read -r line; do
+        sudo rm -rf "$line"/log/*
+        ## fix thinkphp runtime perm
+        sudo chown 33:33 "$line"
+    done
 
 ## clean mysql binary logs
 
