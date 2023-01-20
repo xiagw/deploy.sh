d78a298137e2189ad28cfe59df68af466a1caf37
xiagw <fxiaxiaoyu@gmail.com>
2023-01-20 14:13:26 +0800

    :sparkles: win install ssh add key from github

--- bin/install.ssh.ps1
+++ bin/install.ssh.ps1
@@ -1,5 +1,7 @@
 # Get-ExecutionPolicy -List #查看当前的执行策略
 # Set-ExecutionPolicy -Scope CurrentUser RemoteSigned #设置执行策略为要求远程脚本签名，范围为当前用户
+# irm https://github.com/xiagw/deploy.sh/raw/main/bin/install.ssh.ps1 | iex
+# irm https://gitee.com/xiagw/deploy.sh/raw/main/bin/install.ssh.ps1 | iex
 
 Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH*'
 
@@ -22,8 +24,12 @@ if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyCon
 } else {
     Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
 }
-# 默认 shell 设置为 powershell.exe：
+## 默认 shell 设置为 powershell.exe：
 New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -PropertyType String -Force
+## comment authorized_keys
+(Get-Content -Path C:\ProgramData\ssh\sshd_config -Raw) -replace 'Match Group administrators','#Match Group administrators' | Set-Content -Path C:\ProgramData\ssh\sshd_config
+(Get-Content -Path C:\ProgramData\ssh\sshd_config -Raw) -replace 'AuthorizedKeysFile __PROGRAMDATA__','#AuthorizedKeysFile __PROGRAMDATA__' | Set-Content -Path C:\ProgramData\ssh\sshd_config
+
 Restart-Service sshd
 
 # By default the ssh-agent service is disabled. Allow it to be manually started for the next step to work.
@@ -42,7 +48,7 @@ Restart-Service sshd
 # scp C:\Users\username\.ssh\id_ed25519.pub user1@domain1@contoso.com:C:\Users\username\.ssh\authorized_keys
 
 New-Item -Path $HOME\.ssh -Type Directory -Force
-echo '<pub_key>' >$HOME\.ssh\authorized_keys
+(Invoke-WebRequest 'https://api.github.com/users/xiagw/keys' | ConvertFrom-Json).key | Set-Content -Path $HOME\.ssh\authorized_keys
 # New-Item -Path "C:\ProgramData\ssh\administrators_authorized_keys" -Type File -Force
 # icacls.exe "C:\ProgramData\ssh\administrators_authorized_keys" /inheritance:r /grant "Administrators:F" /grant "SYSTEM:F"
 # echo '<pub_key>' >"C:\ProgramData\ssh\administrators_authorized_keys"
