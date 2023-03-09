cc6d13f33161450ed65cd19daecd1f675cbc6212
xiagw <fxiaxiaoyu@gmail.com>
2023-03-09 22:26:07 +0800

    ⚡️ Dockerfile for java

--- conf/dockerfile/Dockerfile.java
+++ conf/dockerfile/Dockerfile.java
@@ -1,20 +1,26 @@
-FROM maven:3.6-jdk-8 as BUILDER
+FROM maven:3.6-jdk-8 AS builder
 ARG MVN_PROFILE=test
 ARG MVN_DEBUG=-q
-WORKDIR /app
+ARG SETTINGS=https://gitee.com/xiagw/deploy.sh/raw/main/conf/dockerfile/settings.xml
+WORKDIR /src
 COPY . .
 SHELL ["/bin/bash", "-o", "pipefail", "-c"]
 RUN set -xe \
     # && echo "192.168.145.12 mynexus" >>/etc/hosts \
     && [ -d /root/.m2 ] || mkdir -p /root/.m2 \
+    # && curl -Lo /root/.m2/settings.xml $SETTINGS \
     && [ -f docs/settings.xml ] && cp -vf docs/settings.xml /root/.m2/ || true \
     && [ -f settings.xml ] && cp -vf settings.xml /root/.m2/ || true
 RUN set -xe && mvn -T 1C clean -U package -DskipTests -Dmaven.compile.fork=true
-RUN mkdir /jar_file \
-    && find . -type f -regextype egrep -iregex '.*SNAPSHOT.*\.jar' -exec cp {} /jar_file/ \; \
-    && rm -f /jar_file/framework* /jar_file/gdp-module* /jar_file/sdk*.jar /jar_file/*-common-*.jar /jar_file/*-dao-*.jar
+WORKDIR /jar_file
+RUN find /src -type f -regextype egrep -iregex '.*SNAPSHOT.*\.jar' -exec cp {} ./ \; \
+    && rm -f ./framework* ./gdp-module* sdk*.jar ./*-commom-*.jar ./*-dao-*.jar ./lop-opensdk*.jar ./core-*.jar
 
-FROM openjdk:8u332 as RUNTIME
+#############################
+# FROM openjdk:11-jdk
+# FROM bitnami/tomcat:8.5 as p0
+# FROM bitnami/java:1.8-prod as p0
+FROM openjdk:8u332
 ## set startup profile
 ARG MVN_PROFILE=test
 ## Set Timezone
@@ -28,13 +34,14 @@ ENV JAVA_OPTS="$JAVA_OPTS"
 ARG INSTALL_FONTS=false
 ## install ffmpeg
 ARG INSTALL_FFMPEG=false
+ARG RUNSH=https://gitee.com/xiagw/deploy.sh/raw/main/conf/dockerfile/run.sh
+ARG FONTS=http://cdn.flyh6.com/docker/fonts-2022.tgz
 
-RUN set -x; \
-    if [ "$INSTALL_FONTS" = true ]; then \
+RUN if [ "$INSTALL_FONTS" = true ]; then \
     sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list \
     && apt-get update && apt-get install -y --no-install-recommends fontconfig \
     && fc-cache --force \
-    && curl -Lo /tmp/fonts.tgz --referer http://www.flyh6.com/ http://cdn.flyh6.com/docker/fonts-2022.tgz \
+    && curl -Lo /tmp/fonts.tgz --referer http://www.flyh6.com/ $FONTS \
     && tar -zxf /tmp/fonts.tgz -C /usr/share \
     && rm -f /tmp/fonts.tgz; \
     fi; \
@@ -43,17 +50,18 @@ RUN set -x; \
     fi; \
     ## set ssl
     sed -i 's/SSLv3\,\ TLSv1\,\ TLSv1\.1\,//g' /usr/local/openjdk-8/jre/lib/security/java.security || true; \
-    curl -Lo /opt/run.sh https://gitee.com/xiagw/deploy.sh/raw/main/conf/dockerfile/run.sh; \
-    chmod +x /opt/run.sh; \
+    useradd -u 1000 spring \
+    && curl -Lo /opt/run.sh $RUNSH \
+    && chmod +x /opt/run.sh \
     # apt-get autoremove -y \
-    apt-get clean all \
+    && apt-get clean all \
     && rm -rf /var/lib/apt/lists/*
 
 WORKDIR /app
-COPY ./jar_file/ .
+COPY --from=builder /jar_file/ .
+# COPY ./jar_file/ .
 RUN chown -R 1000:1000 /app \
-    && touch "/app/profile.${MVN_PROFILE}" \
-    && [ -f run.sh ] && chmod +x run.sh || true
+    && touch "/app/profile.${MVN_PROFILE}"
 
 USER 1000
 EXPOSE 8080 8081 8082
