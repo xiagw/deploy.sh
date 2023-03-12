eec367ddc9d7839eab030f05f643edcecc5d00bc
xiagw <fxiaxiaoyu@gmail.com>
2023-03-12 23:05:15 +0800

    add linux oomscore

---
+++ bin/lin.oomscore.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+find /proc -maxdepth 1 -regex '/proc/[0-9]+' | while read -r proc; do
+    printf "%2d      %5d       %s\n" \
+        "$(cat $proc/oom_score)" \
+        "$(basename $proc)" \
+        "$(cat $proc/cmdline | tr '\0' ' ' | head -c 50)"
+done 2>/dev/null | sort -nr | head -n 10
