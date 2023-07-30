f4a527fc57b7ce4bfb0472ef572037298399ab6e
xiagw <fxiaxiaoyu@gmail.com>
2023-07-31 00:18:05 +0800

    🎨 fix build.base.sh

--- conf/dockerfile/build.base.sh
+++ conf/dockerfile/build.base.sh
@@ -2,21 +2,22 @@
 
 set -xe
 if [[ -z "$1" ]]; then
-    tags=(5.6 7.1 7.4 8.1 8.2)
+    vers=(5.6 7.1 7.4 8.1 8.2)
 else
-    tags=("$1")
+    vers=("$1")
 fi
-for tag in "${tags[@]}"; do
+
+for ver in "${vers[@]}"; do
     DOCKER_BUILDKIT=0 docker build \
         -f Dockerfile.php \
-        -t deploy/base:fly-php"${tag/./}" \
-        --build-arg PHP_VERSION="$tag" \
+        -t deploy/base:fly-php"${ver/./}" \
+        --build-arg PHP_VERSION="$ver" \
         --build-arg IN_CHINA="true" \
         .
 
-    echo "FROM deploy/base:fly-php${tag/./}" >Dockerfile
+    echo "FROM deploy/base:fly-php${ver/./}" >Dockerfile
     DOCKER_BUILDKIT=0 docker build \
-        --build-arg=BASE_TAG="fly-php${tag/./}" \
-        -t deploy/php:"$tag" \
+        --build-arg=BASE_TAG="fly-php${ver/./}" \
+        -t deploy/php:"$ver" \
         .
 done
