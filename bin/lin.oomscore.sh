afad5dcc713967f119c8674a81ad4c3d01644bbe
xiagw <fxiaxiaoyu@gmail.com>
2023-08-19 10:06:44 +0800

    ⚡️ improve oomscore.sh

--- bin/lin.oomscore.sh
+++ bin/lin.oomscore.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-find /proc -maxdepth 1 -regex '/proc/[0-9]+' |
-    while read -r proc; do
-        printf "%2d      %5d       %s\n" \
-            "$(cat "$proc"/oom_score)" \
-            "$(basename "$proc")" \
-            "$(cat $proc/cmdline | tr '\0' ' ' | head -c 50)"
-    done 2>/dev/null | sort -nr | head -n 15
+
+# set -x
+while read -r proc; do
+    printf "%2d      %5d       %s\n" \
+        "$(cat "$proc"/oom_score)" \
+        "$(basename "$proc")" \
+        "$(cat $proc/cmdline | tr '\0' ' ' | head -c 50)"
+done < <(find /proc -maxdepth 1 -regex '/proc/[0-9]+') 2>/dev/null | sort -nr | head -n 15
