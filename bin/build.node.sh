0f6b31aa9269e4163b7e13dff5d574fbc77d9765
xiagw <fxiaxiaoyu@gmail.com>
2023-09-24 11:28:11 +0800

    🧑‍💻 fix build node php sh

--- bin/build.node.sh
+++ bin/build.node.sh
@@ -8,17 +8,17 @@ if grep -q "$file_json_md5" "${me_log}"; then
     echo "Same checksum for ${file_json}, skip yarn install."
 else
     echo "New checksum for ${file_json}, run yarn install."
-    YARN_INSTALL=true
+    yarn_install=true
 fi
 if [ ! -d "${gitlab_project_dir}/node_modules" ]; then
-    YARN_INSTALL=true
+    yarn_install=true
 fi
 
 # https://github.com/nodesource/distributions#debinstall
-_msg step "[build] yarn build"
+_msg step "[build] yarn install"
 
 # rm -f package-lock.json
-[[ "${github_action:-0}" -eq 1 ]] && return 0
+${github_action:-false} && return 0
 
 ## create image: deploy/node
 # if ! docker images | grep -q 'deploy/node'; then
@@ -26,44 +26,30 @@ _msg step "[build] yarn build"
 # fi
 
 ## custome build? / 自定义构建？
-if [ -f "$gitlab_project_dir"/custom.build.sh ]; then
-    $run_cmd -v "${gitlab_project_dir}":/app -w /app deploy/node bash custom.build.sh
+if [ -f "$gitlab_project_dir/build.custom.sh" ]; then
+    $run_cmd -v "${gitlab_project_dir}":/app -w /app "${build_image_from:-node:18-slim}" bash build.custom.sh
     return
 fi
 
 ## yarn install (node_modules)
-if [[ ${YARN_INSTALL:-false} == 'true' ]]; then
-    $run_cmd -v "${gitlab_project_dir}":/app -w /app deploy/node bash -c "yarn install" &&
+if ${yarn_install:-false}; then
+    $run_cmd -v "${gitlab_project_dir}":/app -w /app "${build_image_from:-node:18-slim}" bash -c "yarn install" &&
         echo "$file_json_md5" >>"${me_log}"
 else
     _msg time "skip yarn install..."
 fi
 
 ## build
-# "${gitlab_project_dir}/.env.development"
-# "${gitlab_project_dir}/.env.dev"
-# "${gitlab_project_dir}/.env.develop"
-env_dev_files=(
-    "${gitlab_project_dir}/.env.production"
-    "${gitlab_project_dir}/.env.prod"
-    "${gitlab_project_dir}/.env.main"
-    "${gitlab_project_dir}/.env.testing"
-    "${gitlab_project_dir}/.env.test"
-    "${gitlab_project_dir}/.env.uat"
-)
-for env_file in "${env_dev_files[@]}"; do
-    [[ -f $env_file ]] || continue
-    if [[ $env_namespace == *uat* || $env_namespace == *test* ]]; then
-        build_opt=build:stage
-        break
-    fi
-    if [[ $env_namespace == *master* || $env_namespace == *main* || $env_namespace == *prod* ]]; then
-        build_opt=build:prod
-        break
-    fi
-done
+case $env_namespace in
+*uat* | *test*)
+    build_opt=build:stage
+    ;;
+*master* | *main* | *prod*)
+    build_opt=build:prod
+    ;;
+esac
 
-$run_cmd -v "${gitlab_project_dir}":/app -w /app deploy/node bash -c "yarn run ${build_opt:-build}"
+$run_cmd -v "${gitlab_project_dir}":/app -w /app "${build_image_from:-node:18-slim}" bash -c "yarn run ${build_opt:-build}"
 
 if [ -d "${gitlab_project_dir}"/build ]; then
     rsync -a --delete "${gitlab_project_dir}"/build/ "${gitlab_project_dir}"/dist/
--- bin/build.php.sh
+++ bin/build.php.sh
@@ -7,29 +7,30 @@ if grep -q "$file_json_md5" "${me_log}"; then
     _msg time "The same checksum for ${file_json}, skip composer install."
 else
     _msg time "New checksum for ${file_json}, run composer install."
-    COMPOSER_INSTALL=1
+    COMPOSER_INSTALL=true
 fi
 if [ ! -d "${gitlab_project_dir}/vendor" ]; then
     _msg time "Not found ${gitlab_project_dir}/vendor, run composer install."
-    COMPOSER_INSTALL=1
+    COMPOSER_INSTALL=true
 fi
 
-if [ -f "$gitlab_project_dir"/custom.build.sh ]; then
-    _msg time "Found $gitlab_project_dir/custom.build.sh, run it"
-    bash "$gitlab_project_dir"/custom.build.sh
+if [ -f "$gitlab_project_dir"/build.custom.sh ]; then
+    _msg time "Found $gitlab_project_dir/build.custom.sh, run it"
+    bash "$gitlab_project_dir"/build.custom.sh
     return
 fi
 
-if [[ "${COMPOSER_INSTALL:-0}" -eq 1 ]]; then
+if ${COMPOSER_INSTALL:-false}; then
     _msg step "[build] php composer install"
-    [[ "${github_action:-0}" -eq 1 ]] && return 0
+    ${github_action:-false} && return 0
     # if ! docker images | grep -q "deploy/composer"; then
     #     DOCKER_BUILDKIT=1 docker build $quiet_flag --tag "deploy/composer" --build-arg IN_CHINA="${ENV_IN_CHINA}" \
     #         -f "$me_dockerfile/Dockerfile.composer" "$me_dockerfile" >/dev/null
     # fi
     # rm -rf "${gitlab_project_dir}"/vendor
-    # shellcheck disable=2015
-    $docker_run -v "$gitlab_project_dir:/app" -w /app "${build_image_from:-composer}" bash -c "composer install ${quiet_flag}" && echo "$file_json_md5 ${file_json}" >>"${me_log}" || true
+    if $run_cmd -v "$gitlab_project_dir:/app" -w /app "${build_image_from:-composer}" bash -c "composer install ${quiet_flag}"; then
+        echo "$file_json_md5 ${file_json}" >>"${me_log}"
+    fi
     [ -d "$gitlab_project_dir"/vendor ] && chown -R "$(id -u):$(id -g)" "$gitlab_project_dir"/vendor
     _msg stepend "[build] php composer install"
 else
