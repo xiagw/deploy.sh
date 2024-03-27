3df15d6756ecb907f535559ce37a508544d457ec
xiagw <fxiaxiaoyu@gmail.com>
2024-03-27 22:42:19 +0800

    🐞fix(M  conf/dockerfile/Dockerfile.php M  conf/dockerfile/build.base.sh)

--- conf/dockerfile/build.base.sh
+++ conf/dockerfile/build.base.sh
@@ -3,12 +3,6 @@
 set -xe
 me_path="$(dirname "$(readlink -f "$0")")"
 
-if [[ -z "$1" ]]; then
-    vers=(5.6 7.1 7.3 7.4 8.1 8.2)
-else
-    vers=("$1")
-fi
-
 if command -v podman; then
     cmd='podman'
     cmd_opt="$cmd build --progress=plain --force-rm --format=docker"
@@ -16,27 +10,27 @@ else
     cmd='docker'
     cmd_opt="$cmd build --progress=plain"
 fi
+
+if [[ -z "$1" ]]; then
+    vers=(5.6 7.1 7.3 7.4 8.1 8.2)
+else
+    vers=("$1")
+fi
+
+image_repo=registry-vpc.cn-hangzhou.aliyuncs.com/flyh5/flyh5
 for ver in "${vers[@]}"; do
     ## build base
     tag="php-${ver}"
-    $cmd_opt \
+    $cmd_opt -f Dockerfile.php.base \
         --build-arg PHP_VERSION="$ver" \
         --build-arg IN_CHINA="true" \
-        -f Dockerfile.php.base \
-        -t deploy/base:"$tag" \
+        -t $image_repo:"base-$tag" \
         "$me_path"
     ## build for laradock
-    echo "FROM deploy/base:$tag" >Dockerfile.php
-    $cmd_opt \
-        -f Dockerfile.php \
-        -t deploy/php:"$ver" \
+    echo "FROM $image_repo:$tag" >Dockerfile.php
+    $cmd_opt -f Dockerfile.php \
+        -t $image_repo:"php-$ver" \
         "$me_path"
-    ## upload to OSS:
-    $cmd tag deploy/php:"$ver" laradock-php-fpm
-    $cmd save laradock-php-fpm | gzip -c >/tmp/laradock-php-fpm."$ver".tar.gz
-    select b in $(ossutil ls -s | grep '^oss:') quit; do
-        [ "$b" = quit ] && break
-        ossutil cp /tmp/laradock-php-fpm."$ver".tar.gz "$b"/docker/ -f
-        break
-    done
+
+        docker push $image_repo:"php-$ver"
 done
