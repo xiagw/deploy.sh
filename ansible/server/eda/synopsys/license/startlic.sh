31e40b39ea0f0c11226077e78cc4f894c8d6da3c
xiagw <fxiaxiaoyu@gmail.com>
2024-05-21 07:36:31 +0800

    👔 fix cds init

--- ansible/server/eda/synopsys/license/startlic.sh
+++ ansible/server/eda/synopsys/license/startlic.sh
@@ -1,26 +1,23 @@
 #!/usr/bin/bash
 
-if [ -d /home/eda ]; then
-    eda_home=/home/eda
-else
-    eda_home=/eda
-fi
+for d in /eda /home/eda /opt/eda; do
+    [ -d "$d" ] && eda_home="$d" && break
+done
+[ -z "$eda_home" ] && echo "Not found EDA directory" && exit 1
 
-# bin_lmgrd=$eda_home/synopsys/scl/2018.06/linux64/bin/lmgrd
-# bin_lmutil=$eda_home/synopsys/scl/2018.06/linux64/bin/lmutil
-# bin_lmstat=$eda_home/synopsys/scl/2018.06/linux64/bin/lmstat
-
-bin_lmgrd=$eda_home/synopsys/scl/2021.03/linux64/bin/lmgrd
-bin_lmutil=$eda_home/synopsys/scl/2021.03/linux64/bin/lmutil
-bin_lmstat=$eda_home/synopsys/scl/2021.03/linux64/bin/lmstat
+# bin_dir=$eda_home/synopsys/scl/2018.06/linux64/bin
+bin_dir=$eda_home/synopsys/scl/2021.03/linux64/bin
+bin_lmgrd=$bin_dir/lmgrd
+bin_lmutil=$bin_dir/lmutil
+bin_lmstat=$bin_dir/lmstat
 
 host_name="$(hostname -s)"
 path_lic=$eda_home/synopsys/license
-file_lic=$path_lic/$host_name.license.dat
-file_log=$path_lic/$host_name.debug.log
-
-if [ ! -w "$file_log" ]; then
-    file_log=$HOME/$host_name.debug.log
+file_lic=$path_lic/${host_name}.license.dat
+if [ -w "$path_lic" ]; then
+    file_log=$path_lic/${host_name}.debug.log
+else
+    file_log=$HOME/${host_name}.debug.log
 fi
 
 for file in "$file_lic" $bin_lmgrd $bin_lmutil $bin_lmstat; do
@@ -48,6 +45,6 @@ status)
     $bin_lmstat
     ;;
 *)
-    echo "Usage: $(basename "$0") [ start | stop | restart | help ]"
+    echo "Usage: $(basename "$0") [ start | stop | restart ]"
     ;;
 esac
