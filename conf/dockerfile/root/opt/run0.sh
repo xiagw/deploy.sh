ac7d50e86f052f1a39bd8f830086f5ef71b29079
xiagw <fxiaxiaoyu@gmail.com>
2024-05-31 20:20:13 +0800

    ♻️ fix run0.sh

--- conf/dockerfile/root/opt/run0.sh
+++ conf/dockerfile/root/opt/run0.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-## 初始化程序
+## 需要root权限的初始化程序
 [ -f /opt/init.sh ] && bash /opt/init.sh
 [ -f /app/init.sh ] && bash /app/init.sh
 
+## 需要普通账号启动的程序
 if id spring; then
     su -l spring -c "export LANG=C.UTF-8; bash /opt/run.sh"
 elif id node; then
     su -l node -c "export LANG=C.UTF-8; bash /opt/run.sh"
 else
-    echo "No user found"
+    bash /opt/run.sh
 fi
