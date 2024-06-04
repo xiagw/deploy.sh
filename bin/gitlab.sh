63d95c3f0286b42c9c5f39d96b4a1124b27cfcbc
xiagw <fxiaxiaoyu@gmail.com>
2024-06-04 15:45:04 +0800

    🐞fix(M  bin/gitlab.sh)

--- bin/gitlab.sh
+++ bin/gitlab.sh
@@ -3,7 +3,7 @@
 
 _add_account() {
 
-    if $cmd_gitlab user list --username "$user_name" | jq -r '.[].name' | grep -q -m "$user_name"; then
+    if $cmd_gitlab user list --username "$user_name" | jq -r '.[].name' | grep -q -w "$user_name"; then
         if _get_yes_no "user $user_name exists, update $user_name password?"; then
             user_id=$($cmd_gitlab user list --username "$user_name" | jq -r '.[].id')
             $cmd_gitlab user update --id "${user_id}" --username "$user_name" --password "${password_rand:? empty password}" --name "$user_name" --email "$user_name@${gitlab_domain}" --skip-reconfirmation 1
