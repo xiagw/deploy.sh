879a502624d12d983ea24cf6f9536a8f271cdbb4
xiagw <fxiaxiaoyu@gmail.com>
2024-06-11 11:49:14 +0800

    🧐 fix Dockerfile.nginx

--- conf/dockerfile/root/opt/build.sh
+++ conf/dockerfile/root/opt/build.sh
@@ -278,21 +278,14 @@ _build_node() {
 _build_maven() {
     # --settings=settings.xml --activate-profiles=main
     # mvn -T 1C install -pl $moduleName -am --offline
-    mvn_opt+=(
-        mvn
-        --threads 1C
-        --update-snapshots
-        --define skipTests
-        --define maven.compile.fork=true
-        --define user.home=/var/maven
-    )
+    mvn_opt="mvn --threads 1C --update-snapshots --define skipTests --define maven.compile.fork=true --define user.home=/var/maven"
     if [ "$MVN_DEBUG" = off ]; then
-        mvn_opt+=(--quiet)
+        mvn_opt="$mvn_opt --quiet"
     fi
     if [ -f /root/.m2/settings.xml ]; then
-        mvn_opt+=(--settings=/root/.m2/settings.xml)
+        mvn_opt="$mvn_opt --settings=/root/.m2/settings.xml"
     fi
-    "${mvn_opt[@]}" clean package
+    $mvn_opt clean package
     jars_dir=/jars
     mkdir $jars_dir
     while read -r jar; do
