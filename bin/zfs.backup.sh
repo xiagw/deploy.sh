0884a1a7f07323cc9dd530f694465fcb6aedacfe
xiagw <fxiaxiaoyu@gmail.com>
2024-10-08 19:29:22 +0800

    📦 fix zfs backup

--- bin/zfs.backup.sh
+++ bin/zfs.backup.sh
@@ -1,47 +1,52 @@
 #!/usr/bin/env bash
 
 _backup_zfs() {
-    date
-    zfs_src=zfs01/share
-    zfs_dest=zfs02/share
+    date +%Y%m%d-%u-%T.%3N
+    local zfs_src="${1:-zfs01/share}"
+    local zfs_dest="${2:-zfs02/share}"
+    local snap
+    snap="$(date +%s)"
     zfs list -t snapshot
-    snapshot_date="$(date +%s)"
-    if zfs list -t snapshot -o name -s creation -H -r ${zfs_src} | grep -q "${zfs_src}@now$"; then
-        if zfs list -t snapshot -o name -s creation -H -r ${zfs_src} | grep -q "${zfs_src}@last$"; then
-            zfs rename ${zfs_src}@last "${zfs_src}@${snapshot_date}"
+
+    if zfs list -t snapshot -o name -s creation -H -r "${zfs_src}@now"; then
+        if zfs list -t snapshot -o name -s creation -H -r "${zfs_src}@last"; then
+            zfs rename "${zfs_src}@last" "${zfs_src}@${snap}"
         fi
-        zfs rename ${zfs_src}@now ${zfs_src}@last
+        zfs rename "${zfs_src}@now" "${zfs_src}@last"
     fi
-    if zfs list -t snapshot -o name -s creation -H -r ${zfs_dest} | grep -q "${zfs_dest}@now$"; then
-        if zfs list -t snapshot -o name -s creation -H -r ${zfs_dest} | grep -q "${zfs_dest}@last$"; then
-            zfs rename ${zfs_dest}@last "${zfs_dest}@${snapshot_date}"
+    if zfs list -t snapshot -o name -s creation -H -r "${zfs_dest}@now"; then
+        if zfs list -t snapshot -o name -s creation -H -r "${zfs_dest}@last"; then
+            zfs rename "${zfs_dest}@last" "${zfs_dest}@${snap}"
         fi
-        zfs rename ${zfs_dest}@now ${zfs_dest}@last
+        zfs rename "${zfs_dest}@now" "${zfs_dest}@last"
     fi
 
-    zfs snapshot ${zfs_src}@now
+    zfs snapshot "${zfs_src}@now"
 
-    if zfs list -t snapshot -o name -s creation -H -r ${zfs_src} | grep -q "${zfs_src}@last$"; then
-        if command -v pv >/dev/null 2>&1; then
-            zfs send -i ${zfs_src}@last ${zfs_src}@now | pv | zfs recv ${zfs_dest}
+    if command -v pv >/dev/null 2>&1; then
+        local has_pv=1
+    fi
+    if zfs list -t snapshot -o name -s creation -H -r "${zfs_src}@last"; then
+        if [[ "$has_pv" -eq 1 ]]; then
+            zfs send -v -i "${zfs_src}@last" "${zfs_src}@now" | pv | zfs recv "${zfs_dest}"
         else
-            zfs send -i ${zfs_src}@last ${zfs_src}@now | zfs recv ${zfs_dest}
+            zfs send -v -i "${zfs_src}@last" "${zfs_src}@now" | zfs recv "${zfs_dest}"
         fi
     else
         ## full backup
-        zfs snapshot ${zfs_src}@last
-        if command -v pv >/dev/null 2>&1; then
-            zfs send ${zfs_src}@last | pv | zfs recv ${zfs_dest}
+        zfs snapshot "${zfs_src}@last"
+        if [[ "$has_pv" -eq 1 ]]; then
+            zfs send -v "${zfs_src}@last" | pv | zfs recv "${zfs_dest}"
         else
-            zfs send ${zfs_src}@last | zfs recv ${zfs_dest}
+            zfs send -v "${zfs_src}@last" | zfs recv "${zfs_dest}"
         fi
     fi
     ## clear snapshot
-    zfs destroy "${zfs_src}@${snapshot_date}"
-    zfs destroy "${zfs_dest}@${snapshot_date}"
-    date
+    zfs destroy "${zfs_src}@${snap}"
+    zfs destroy "${zfs_dest}@${snap}"
+    date +%Y%m%d-%u-%T.%3N
 }
 
 set -e
 
-_backup_zfs
+_backup_zfs "$@"
