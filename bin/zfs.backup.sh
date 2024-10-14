b5bf1a752a24e494bf4be5924ba3aae8c90c6e28
xiagw <fxiaxiaoyu@gmail.com>
2024-10-14 12:05:09 +0800

    fix zfs backup

--- bin/zfs.backup.sh
+++ bin/zfs.backup.sh
@@ -1,26 +1,24 @@
 #!/usr/bin/env bash
 
 _backup_zfs() {
-    date +%Y%m%d-%u-%T.%3N
     local zfs_src="${1:-zfs01/share}"
     local zfs_dest="${2:-zfs02/share}"
     local snap
     snap="$(date +%s)"
+    local start_time
+    start_time=$(date +%Y%m%d-%u-%T.%3N)
+
+    echo "Starting backup at $start_time"
     zfs list -t snapshot
 
     # Rename existing snapshots
-    if zfs list -t snapshot -o name -s creation -H -r "${zfs_src}@now"; then
-        if zfs list -t snapshot -o name -s creation -H -r "${zfs_src}@last"; then
-            zfs rename "${zfs_src}@last" "${zfs_src}@${snap}"
+    for fs in "$zfs_src" "$zfs_dest"; do
+        if zfs list -t snapshot -o name -s creation -H -r "${fs}@now"; then
+            zfs list -t snapshot -o name -s creation -H -r "${fs}@last" &&
+                zfs rename "${fs}@last" "${fs}@${snap}"
+            zfs rename "${fs}@now" "${fs}@last"
         fi
-        zfs rename "${zfs_src}@now" "${zfs_src}@last"
-    fi
-    if zfs list -t snapshot -o name -s creation -H -r "${zfs_dest}@now"; then
-        if zfs list -t snapshot -o name -s creation -H -r "${zfs_dest}@last"; then
-            zfs rename "${zfs_dest}@last" "${zfs_dest}@${snap}"
-        fi
-        zfs rename "${zfs_dest}@now" "${zfs_dest}@last"
-    fi
+    done
 
     # Create new snapshot
     zfs snapshot "${zfs_src}@now"
@@ -47,12 +45,12 @@ _backup_zfs() {
         fi
     fi
 
-    ## clear snapshot
-    zfs destroy "${zfs_src}@${snap}"
-    zfs destroy "${zfs_dest}@${snap}"
-    date +%Y%m%d-%u-%T.%3N
+    # Clear snapshots
+    zfs destroy "${zfs_src}@${snap}" "${zfs_dest}@${snap}"
+
+    echo "Backup completed at $(date +%Y%m%d-%u-%T.%3N)"
+    echo "Total duration: $(($(date +%s) - snap)) seconds"
 }
 
 set -e
-
 _backup_zfs "$@"
