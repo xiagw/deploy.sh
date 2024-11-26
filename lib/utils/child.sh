e453ed7bf038bf080d735f484d65f6ccde04e3b0
xiagw <fxiaxiaoyu@gmail.com>
2024-11-26 15:33:36 +0800

    fix child.sh

--- lib/utils/child.sh
+++ lib/utils/child.sh
@@ -18,45 +18,36 @@ _validate_time_format() {
         _log "无效的时间格式: ${time_str}"
         return 1
     fi
-    return 0
 }
 
 _get_minutes_elapsed() {
-    local timestamp_file=$1
-    local file_time
+    local type=$1
     local current_time
+    local time_str
+    local time_seconds
 
-    if [[ ! -f ${timestamp_file} ]]; then
-        _log "文件不存在: ${timestamp_file}"
+    if [[ ${type} == "play" ]]; then
+        time_str=$(awk -F= '/^play_time=/{print $2}' "${file_status}")
+    else
+        time_str=$(awk -F= '/^rest_time=/{print $2}' "${file_status}")
+    fi
+
+    if [[ -z ${time_str} ]] || ! _validate_time_format "${time_str}"; then
+        _log "无效的时间格式: ${time_str}"
         return 1
     fi
 
-    if ! _validate_time_format "$(cat "${timestamp_file}")"; then
-        _log "更新无效的时间文件: ${timestamp_file}"
-        date +"%F %T" > "${timestamp_file}"
-    fi
-
-    file_time=$(date +%s -d "$(cat "${timestamp_file}")")
     current_time=$(date +%s)
-    echo $(((current_time - file_time) / 60))
-}
-
-_ensure_file_permissions() {
-    local file=$1
-    if [[ -f ${file} ]]; then
-        chmod 644 "${file}" || _log "无法设置文件权限: ${file}"
-    fi
+    time_seconds=$(date +%s -d "${time_str}")
+    echo $(((current_time - time_seconds) / 60))
 }
 
 _do_shutdown() {
     local reason=$1
     if [[ ${debug_mod:-0} == 1 ]]; then
         _log "DEBUG模式: 触发关机条件: ${reason}"
-        if [[ -f ${file_play} ]]; then
-            _log "DEBUG模式: 显示启动时间文件内容: $(cat "${file_play}")"
-        fi
-        if [[ -f ${file_rest} ]]; then
-            _log "DEBUG模式: 显示关机时间文件内容: $(cat "${file_rest}")"
+        if [[ -f ${file_status} ]]; then
+            _log "DEBUG模式: 显示状态文件内容: $(cat "${file_status}")"
         fi
         return 0
     fi
@@ -87,7 +78,7 @@ _check_time_limits() {
     return 0
 }
 
-_trigger() {
+_remote_trigger() {
     if curl -fssSL -X POST "${URL_HOST}/trigger" 2>/dev/null | grep -qi "rest"; then
         _do_shutdown "收到远程关机命令"
         return 0
@@ -96,15 +87,11 @@ _trigger() {
 }
 
 _reset() {
-    local result=0
-    if [[ -f ${file_play} ]]; then
-        rm -f "${file_play}" || result=1
-    fi
-    if [[ -f ${file_rest} ]]; then
-        rm -f "${file_rest}" || result=1
+    if [[ -f ${file_status} ]]; then
+        rm -f "${file_status}" || return 1
     fi
     sudo shutdown -c || true
-    return "${result}"
+    return 0
 }
 
 main() {
@@ -121,55 +108,46 @@ main() {
     WORK_HOUR_21=21
     DELAY_SECONDS=60
     URL_HOST="http://192.168.5.1"
-    URL_PORT=8899
 
     # 文件路径
-    file_play="${SCRIPT_PATH}/${SCRIPT_NAME}.play"
-    file_rest="${SCRIPT_PATH}/${SCRIPT_NAME}.rest"
+    file_status="${SCRIPT_PATH}/${SCRIPT_NAME}.status"
 
     # 命令处理
     case $1 in
-    reset | r)
-        _reset
-        return
-        ;;
-    debug | d)
-        debug_mod=1
-        ;;
+    reset | r) _reset && return ;;
+    debug | d) debug_mod=1 ;;
     esac
 
     # 检查时间限制
     _check_time_limits || return
 
-    # 触发检查
-    _trigger
+    # 远程触发检查
+    _remote_trigger
 
-    # 创建启动时间文件
-    if [[ ! -f ${file_play} ]]; then
-        date +"%F %T" > "${file_play}"
-        _ensure_file_permissions "${file_play}"
-    fi
-
-    # 创建关机时间文件
-    if [[ ! -f ${file_rest} ]]; then
-        date -d "120 minutes ago" +"%F %T" > "${file_rest}"
-        _ensure_file_permissions "${file_rest}"
+    # 初始化状态文件
+    if [[ ! -f ${file_status} ]]; then
+        {
+            echo "play_time=$(date +"%F %T")"
+            echo "rest_time=$(date -d "120 minutes ago" +"%F %T")"
+        } > "${file_status}"
     fi
 
     # 检查时间限制
     local rest_elapsed play_elapsed
-    rest_elapsed=$(_get_minutes_elapsed "${file_rest}")
-    play_elapsed=$(_get_minutes_elapsed "${file_play}")
+    rest_elapsed=$(_get_minutes_elapsed "rest")
+    play_elapsed=$(_get_minutes_elapsed "play")
 
     # 检查是否需要更新启动时间
-    if [[ -f ${file_rest} ]]; then
-        local play_time rest_time
-        play_time=$(date +%s -d "$(cat "${file_play}")")
-        rest_time=$(date +%s -d "$(cat "${file_rest}")")
-        if ((play_time < rest_time)); then
-            _log "更新启动时间: $(date +"%F %T") ， before: $(cat "${file_play}")"
-            date +"%F %T" > "${file_play}"
-            _ensure_file_permissions "${file_play}"
+    local play_time rest_time
+    play_time=$(awk -F= '/^play_time=/{print $2}' "${file_status}")
+    rest_time=$(awk -F= '/^rest_time=/{print $2}' "${file_status}")
+
+    if [[ -n ${play_time} && -n ${rest_time} ]]; then
+        play_timestamp=$(date +%s -d "${play_time}")
+        rest_timestamp=$(date +%s -d "${rest_time}")
+        if ((play_timestamp < rest_timestamp)); then
+            _log "更新启动时间: $(date +"%F %T") ， before: ${play_time}"
+            sed -i "s/^play_time=.*/play_time=$(date +"%F %T")/" "${file_status}"
         fi
     fi
 
@@ -181,8 +159,7 @@ main() {
 
     # 检查开机时长
     if ((play_elapsed >= PLAY_MINUTES)); then
-        date +"%F %T" > "${file_rest}"
-        _ensure_file_permissions "${file_rest}"
+        sed -i "s/^rest_time=.*/rest_time=$(date +"%F %T")/" "${file_status}"
         _do_shutdown "开机时间超过${PLAY_MINUTES}分钟"
         return
     fi
