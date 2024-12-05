b8137df050dd9efad55ab28077b10b4c0997138b
xiagw <fxiaxiaoyu@gmail.com>
2024-12-05 20:35:11 +0800

    Refactor openwrt.sh to include root partition and filesystem resizing scripts; remove obsolete expand_root script

--- bin/openwrt.sh
+++ bin/openwrt.sh
@@ -33,8 +33,59 @@ opkg update
 opkg install wireguard-tools xray-core
 opkg install parted losetup resize2fs
 
-wget -U "" -O expand-root.sh "https://openwrt.org/_export/code/docs/guide-user/advanced/expand_root?codeblock=0"
-. ./expand-root.sh
+# wget -U "" -O expand-root.sh "https://openwrt.org/_export/code/docs/guide-user/advanced/expand_root?codeblock=0"
+# . ./expand-root.sh
+
+# Configure startup scripts
+cat <<"EOF" >/etc/uci-defaults/70-rootpt-resize
+if [ ! -e /etc/rootpt-resize ] \
+&& type parted > /dev/null \
+&& lock -n /var/lock/root-resize
+then
+ROOT_BLK="$(readlink -f /sys/dev/block/"$(awk -e \
+'$9=="/dev/root"{print $3}' /proc/self/mountinfo)")"
+ROOT_DISK="/dev/$(basename "${ROOT_BLK%/*}")"
+ROOT_PART="${ROOT_BLK##*[^0-9]}"
+parted -f -s "${ROOT_DISK}" \
+resizepart "${ROOT_PART}" 100%
+mount_root done
+touch /etc/rootpt-resize
+reboot
+fi
+exit 1
+EOF
+
+cat <<"EOF" >/etc/uci-defaults/80-rootfs-resize
+if [ ! -e /etc/rootfs-resize ] \
+&& [ -e /etc/rootpt-resize ] \
+&& type losetup > /dev/null \
+&& type resize2fs > /dev/null \
+&& lock -n /var/lock/root-resize
+then
+ROOT_BLK="$(readlink -f /sys/dev/block/"$(awk -e \
+'$9=="/dev/root"{print $3}' /proc/self/mountinfo)")"
+ROOT_DEV="/dev/${ROOT_BLK##*/}"
+LOOP_DEV="$(awk -e '$5=="/overlay"{print $9}' \
+/proc/self/mountinfo)"
+if [ -z "${LOOP_DEV}" ]
+then
+LOOP_DEV="$(losetup -f)"
+losetup "${LOOP_DEV}" "${ROOT_DEV}"
+fi
+resize2fs -f "${LOOP_DEV}"
+mount_root done
+touch /etc/rootfs-resize
+reboot
+fi
+exit 1
+EOF
+
+if ! grep -qE "70-rootpt-resize" /etc/sysupgrade.conf; then
+    echo '/etc/uci-defaults/70-rootpt-resize' >>/etc/sysupgrade.conf
+fi
+if ! grep -qE "80-rootfs-resize" /etc/sysupgrade.conf; then
+    echo '/etc/uci-defaults/80-rootfs-resize' >>/etc/sysupgrade.conf
+fi
 
 # Expand root partition/filesystem
 sh /etc/uci-defaults/70-rootpt-resize
