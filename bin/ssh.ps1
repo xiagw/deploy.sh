a00af8b742b0aeb56815eb5e7e2b2be606bde0e2
xiagw <fxiaxiaoyu@gmail.com>
2024-12-09 09:25:42 +0800

    🐞fix(M  bin/ssh.ps1)

--- bin/ssh.ps1
+++ bin/ssh.ps1
@@ -192,8 +192,7 @@ function Install-OpenSSH {
     }
 
     # …Ë÷√ PowerShell Œ™ƒ¨»œ shell
-    New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell `
-        -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Force
+    New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Force
 
     # ≈‰÷√ SSH √‹‘ø
     $sshPaths = @{
