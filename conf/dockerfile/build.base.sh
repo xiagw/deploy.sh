a32119ea33ef574bcb01f3c48fa4deed69fe1cc4
xiagw <fxiaxiaoyu@gmail.com>
2025-03-12 10:30:57 +0800

    Refactor base image build script with improved flexibility and multi-platform support
    - Rename `_build()` function to `build_base_image()` for clarity
    - Add support for Node.js 22 version
    - Improve Docker/Podman command detection and build options
    - Simplify command-line argument handling
    - Enhance multi-platform image build process with dynamic command selection

--- conf/dockerfile/build.base.sh
+++ conf/dockerfile/build.base.sh
@@ -2,90 +2,90 @@
 # shellcheck disable=SC2207
 set -xe
 
-_build() {
-    local v="$1"
-    local reg=registry.cn-hangzhou.aliyuncs.com/flyh5/flyh5
-    case "$v" in
+build_base_image() {
+    local ver="$1" reg cmd cmd_opt
+    local reg='registry.cn-hangzhou.aliyuncs.com/flyh5/flyh5'
+
+    cmd="$(command -v docker || command -v podman)"
+    cmd_opt=(
+        "$cmd"
+        build
+        --progress=plain
+        --push
+        --platform "linux/amd64,linux/arm64"
+        --build-arg CHANGE_SOURCE=true
+        --build-arg IN_CHINA=true
+        --build-arg HTTP_PROXY="${http_proxy-}"
+        --build-arg HTTPS_PROXY="${http_proxy-}"
+    )
+
+    case "$ver" in
     5.6 | 7.1 | 7.3 | 7.4 | 8.1 | 8.2 | 8.3 | 8.4)
         cmd_opt+=(
+            --build-arg PHP_VERSION="$ver"
             -f Dockerfile.php.base
-            --tag "$reg":laradock-php-fpm-"$v"
-            --build-arg PHP_VERSION="$v"
+            --tag "$reg":laradock-php-fpm-"$ver"
         )
         ;;
     redis)
         cmd_opt+=(
-            -f Dockerfile."$v"
-            --tag "$reg":laradock-"$v"
+            -f Dockerfile."$ver"
+            --tag "$reg":laradock-"$ver"
         )
         ;;
     nginx)
         cmd_opt+=(
-            -f Dockerfile."$v"
-            --tag "$reg":"$v"-alpine-base
+            -f Dockerfile."$ver"
+            --tag "$reg":"$ver"-alpine-base
         )
         ;;
     mysql-5.6 | mysql-5.7 | mysql-8.0 | mysql-8.4)
         cmd_opt+=(
+            --build-arg MYSQL_VERSION="${ver#*-}"
             -f Dockerfile.mysql
-            --tag "$reg":laradock-"$v"
-            --build-arg MYSQL_VERSION="${v#*-}"
+            --tag "$reg":laradock-"$ver"
         )
         ;;
     spring-8 | spring-17 | spring-21)
         cmd_opt+=(
+            --build-arg JDK_VERSION="${ver#*-}"
             -f Dockerfile.java.base
-            --tag "$reg":laradock-"$v"
-            --build-arg JDK_VERSION="${v#*-}"
+            --tag "$reg":laradock-"$ver"
         )
         ;;
-    nodejs-18 | nodejs-20 | nodejs-21)
+    nodejs-18 | nodejs-20 | nodejs-21 | nodejs-22)
         cmd_opt+=(
+            --build-arg NODE_VERSION="${ver#*-}"
             -f Dockerfile.node.base
-            --tag "$reg":laradock-"$v"
-            --build-arg NODE_VERSION="${v#*-}"
+            --tag "$reg":laradock-"$ver"
         )
         ;;
     esac
 
     # https://docs.docker.com/build/building/multi-platform/#build-multi-platform-images
     if ! ls /proc/sys/fs/binfmt_misc/qemu-aarch64; then
-        docker run --privileged --rm tonistiigi/binfmt --install all
+        $cmd run --privileged --rm tonistiigi/binfmt --install all
     fi
 
     # echo "${cmd_opt[@]} $me_path/"
     "${cmd_opt[@]}" "$me_path/"
 }
 
-cmd_opt+=(
-    $(if command -v docker; then
-        echo build
-    elif command -v podman; then
-        echo build --force-rm --format=docker
-    fi)
-    --progress=plain
-    --platform "linux/amd64,linux/arm64"
-    --build-arg CHANGE_SOURCE=true
-    --build-arg IN_CHINA=true
-    --build-arg HTTP_PROXY="${http_proxy-}"
-    --build-arg HTTPS_PROXY="${http_proxy-}"
-    --push
-)
+cmd_arg="${*}"
+all_args=(5.6 7.1 7.3 7.4 8.1 8.2 8.3 8.4 mysql-5.6 mysql-5.7 mysql-8.0 mysql-8.4 spring-8 spring-17 spring-21 nodejs-18 nodejs-20 nodejs-21 redis nginx)
 
 me_path="$(dirname "$(readlink -f "$0")")"
-args=(5.6 7.1 7.3 7.4 8.1 8.2 8.3 8.4 mysql-5.6 mysql-5.7 mysql-8.0 mysql-8.4 spring-8 spring-17 spring-21 nodejs-18 nodejs-20 nodejs-21 redis nginx)
-arg="${1:-}"
 
-case "$arg" in
+case "$cmd_arg" in
 all)
-    for i in "${args[@]}"; do
-        _build "$i"
+    for i in "${all_args[@]}"; do
+        build_base_image "$i"
     done
     ;;
 *)
-    if [ -z "$arg" ]; then
-        arg=$(echo "${args[@]}" | sed 's/\ /\n/g' | fzf)
+    if [ -z "$cmd_arg" ]; then
+        select_arg=$(echo "${all_args[@]}" | sed 's/\ /\n/g' | fzf)
     fi
-    _build "$arg"
+    build_base_image "$select_arg"
     ;;
 esac
