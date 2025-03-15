974e13952c55a6aa8cfd994447071fc570307f17
xiagw <fxiaxiaoyu@gmail.com>
2025-03-15 22:26:39 +0800

    Refactor Dockerfile and build.sh to simplify build stages and remove pre-download dependency logic from Maven build process.

--- conf/dockerfile/Dockerfile.java
+++ conf/dockerfile/Dockerfile.java
@@ -1,30 +1,14 @@
-#### docker build stage 1: Dependencies ####
+#### docker build stage 1 ####
 ARG MVN_IMAGE=maven
 ARG MVN_VERSION=3.8-amazoncorretto-8
 ARG JDK_IMAGE=amazoncorretto
 ARG JDK_VERSION=8
-
-FROM ${MVN_IMAGE}:${MVN_VERSION} AS deps
+FROM ${MVN_IMAGE}:${MVN_VERSION} AS builder
 ARG IN_CHINA=false
 ARG MVN_PROFILE=main
 ARG MVN_DEBUG=off
 ARG BUILD_URL=https://gitee.com/xiagw/deploy.sh/raw/main/conf/dockerfile/root/opt/build.sh
 WORKDIR /src
-# 预下载依赖
-RUN --mount=type=cache,target=/root/.m2 \
-    --mount=type=bind,target=/src,rw \
-    if [ -f /src/root/opt/build.sh ]; then bash /src/root/opt/build.sh deps; \
-    elif [ -f build.sh ]; then bash build.sh deps; \
-    else curl -fLo build.sh $BUILD_URL; bash build.sh deps; fi
-
-#### docker build stage 2: Build ####
-FROM deps AS builder
-ARG IN_CHINA=false
-ARG MVN_PROFILE=main
-ARG MVN_DEBUG=off
-ARG BUILD_URL=https://gitee.com/xiagw/deploy.sh/raw/main/conf/dockerfile/root/opt/build.sh
-WORKDIR /src
-# 构建应用
 RUN --mount=type=cache,target=/root/.m2 \
     --mount=type=bind,target=/src,rw \
     if [ -f /src/root/opt/build.sh ]; then bash /src/root/opt/build.sh; \
