187442ede12d566df12d13e779b2f9e7b70d29ba
xiagw <fxiaxiaoyu@gmail.com>
2025-03-21 18:52:29 +0800

    Remove obsolete scripts and configuration files, including helm_new.sh, various cloud configuration files, and build.base.sh, to streamline the codebase and eliminate unused components.

--- bin/mysql.sh
+++ bin/mysql.sh
@@ -2,22 +2,11 @@
 
 create_mysql_user() {
     # check if required ~/.my.cnf is exist
-    select mysql_conf in $HOME/.my.*.cnf quit; do
-        cp -avf $mysql_conf $HOME/.my.cnf
+    select mysql_conf in $HOME/.my.cnf $HOME/.my.*.cnf quit; do
+        cp -avf "$mysql_conf" "$HOME/.my.cnf"
         break
     done
-    if [ ! -f $HOME/.my.cnf ]; then
-        echo "Not found $HOME/.my.cnf, exit"
-        return 1
-    fi
-    if command -v mycli >/dev/null; then
-        cmd=mycli
-    elif command -v >/dev/null; then
-        cmd=mysql
-    else
-        echo "Not found mysql cli, exit"
-        return 1
-    fi
+    cmd=$(command -v mycli || command -v mysql || return 1)
     if [[ -z "$1" ]]; then
         read -rp 'Enter MySQL username: ' read_user_name
         user_name=${read_user_name:? ERR: empty user name}
@@ -26,18 +15,16 @@ create_mysql_user() {
     fi
     # generate a random password
     password_rand=$(openssl rand -base64 20 | tr -dc A-Za-z0-9 | head -c14)
-    if [ -z "$password_rand" ]; then
+    [ -z "$password_rand" ] &&
         password_rand="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c14)"
-    fi
     # create user with the random password
     # mysql --defaults-extra-file=/path/to/${server}.cnf -e "CREATE USER '${user_name}'@'%' IDENTIFIED BY '${password_rand}'; GRANT ALL PRIVILEGES ON ${read_db_name}.* TO '${user_name}'@'%';"
-    if $cmd -e 'show create user chenlong;' | grep "$user_name"; then
+    if $cmd -e "show create user $user_name;" | grep "$user_name"; then
         echo -e "\n!!!! User $user_name exist, give up !!!!"
     else
         $cmd -e "CREATE USER '${user_name}'@'%' IDENTIFIED BY '${password_rand}'; GRANT ALL PRIVILEGES ON ${user_name}.* TO '${user_name}'@'%';"
         echo "Username=${user_name}/Password=${password_rand}"
     fi
-
 }
 
 create_mysql_user "$@"
