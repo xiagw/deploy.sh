1501d9897616266b7e52ba5aaeb81eef6f625402
xiagw <fxiaxiaoyu@gmail.com>
2026-01-15 17:33:19 +0800

    修复: 更新文件路径引用，将 lib/ 改为 cloud/
    - 更新 bin/aliyun.sh 中的路径引用
    - 更新 bin/cloud 中的路径引用

--- bin/aliyun.sh
+++ bin/aliyun.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 SCRIPT_DIR_0=$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")
-exec "${SCRIPT_DIR_0}/lib/aliyun/main.sh" "$@"
+exec "${SCRIPT_DIR_0}/cloud/aliyun/main.sh" "$@"
