bf2b3f8a4cfa61cc1f8f7b413113a5c7fbeb5249
xiagw <fxiaxiaoyu@gmail.com>
2026-08-15 02:05:08 +0800

    ✨ feat: add .gitlab-ci.yml template whitelist on runner and skip pms repo deploy

--- deploy.sh
+++ deploy.sh
@@ -381,6 +381,12 @@ main() {
     ## 记录脚本开始执行的时间（用于计算总执行时间）
     SECONDS=0
 
+    ## 探测当前项目: 若为公共模板仓库 pms，则跳过部署流程直接退出（pms 只是模板仓库，不需要部署）
+    if [[ "${CI_PROJECT_NAME:-${CI_PROJECT_DIR##*/}}" == "pms" ]]; then
+        echo "pms 为公共模板仓库，跳过部署"
+        return 0
+    fi
+
     unset G_REPO_DIR G_REPO_NAME G_REPO_NS G_REPO_GROUP_PATH G_REPO_GROUP_PATH_SLUG G_REPO_BRANCH
     unset G_REPO_SHORT_SHA G_NAMESPACE G_IMAGE_TAG G_DOCK G_RUN
     unset IS_CHINA G_DEBUG_ON arg_cron arg_image_retain arg_sub_path
